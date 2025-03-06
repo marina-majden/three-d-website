0bf8567a513ec8b5c6048fc3c35661687e4cab27
Marina Majdenić <marina.majden@gmail.com>
2025-03-06 19:38:14 +0100

    style: add basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ const RotatingCube = () => {
   )
 }
   
-
 const App = () => {
   return (
     <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
