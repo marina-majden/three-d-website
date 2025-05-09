e92d7c75a4492cd0e6775e94ec1d53f1ef2ded0d
Marina Majdenić <marina.majden@gmail.com>
2025-05-10 00:22:15 +0200

    feat: add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
-import Hero from './sections/Hero'
+
 import About from './sections/About';
 import Projects from './sections/Projects'
 /* import Experience from './sections/Experience' */
@@ -11,7 +11,7 @@ const App = () => {
   return (  
     <main className="max-w-7xl mx-auto">
       <Navbar />
-      <Hero />
+
       <About />
       <Projects />
       <Contact />
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,43 +1,66 @@
 import React from 'react'
+import "./Navbar.css";
 import { useState } from 'react';
-import menu from "../assets/menu.svg"
-import close from "../assets/close.svg"
+import menu from "../assets/menu.svg";
+import close from "../assets/close.svg";
+import sun from "../assets/sun.min.svg";
+import moon from "../assets/moon.min.svg";
 import { navLinks } from '../constants';
 
 const NavItems = () => {
-    return ( 
+    return (
         <ul className='nav-ul'>
-            {navLinks.map(({id, href, name}) => (
+            {navLinks.map(({ id, href, name }) => (
                 <li key={id} className='nav-li'>
                     <a href={href} className='nav-li_a'>{name}</a>
                 </li>
             ))}
         </ul>
-     )
+    )
 }
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen)
+    const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
+    const [themeMode, setThemeMode] = useState('light');
+    const [isDark, setIsDark] = useState(false);
+    const themeButton = document.documentElement.querySelector('.toggle-theme input');
 
-    return ( 
+    const toggleTheme = () => {
+        setThemeMode((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+        document.documentElement.dataset.theme = themeMode === 'light' ? 'dark' : 'light';
+        setIsDark((prevIsDark) => !prevIsDark);
+
+    }
+
+
+    return (
         <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className="max-w-7xl mx-auto">
                 <div className="flex justify-between items-center py-5 mx-auto c-space">
-                       <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">Marina</a>
-                       <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label="Toggle menu">
+                    <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">Marina</a>
+                    <label className="toggle-theme">
+                        <input className="input" type="checkbox" onClick={toggleTheme} />
+                        <span className="slider"></span>
+                    </label>
+
+                    {/*    <label className="toggle-theme">
+                        <input className="input" type="checkbox" onClick={toggleTheme} /><img src={isDark ? moon : sun} alt="toggleTheme" />
+                        <span className="slider"></span>
+                    </label> */}
+                    <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex' aria-label="Toggle menu">
                         <img src={isOpen ? close : menu} alt="toggle" className="w-6 h-6" />
-                       </button>
-                       <nav className='sm:flex hidden'>
+                    </button>
+                    <nav className='sm:flex hidden'>
                         <NavItems />
-                       </nav>
+                    </nav>
                 </div>
             </div>
-             <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+            <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
                 <nav className='p-5'><NavItems /></nav>
-             </div>
+            </div>
         </header>
-     );
+    );
 }
- 
-export default Navbar ;
+
+export default Navbar;
