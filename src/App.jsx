a335097e57fba2264dbaaca543a6382cb14d1586
Marina Majdenić <marina.majden@gmail.com>
2025-05-16 17:25:06 +0200

    fix: hero section and 3D object

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
-import Hero from './sections/Heroo';
+import Hero from './sections/Hero';
 import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experience from "./sections/Experience";
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,66 +1,69 @@
-import { PerspectiveCamera } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
-import { Center, OrbitControls } from "@react-three/drei";
-import HackerRoom from '../components/HackerRoom'
-import CanvasLoader from "../components/CanvasLoader";
-import { useMediaQuery } from "react-responsive";
-import { calculateSizes } from '../constants/index';
-import HeroCamera from "../components/HeroCamera";
-import Button from "../components/Button";
-import Pheonix from "../components/Pheonix";
-
-// Math function enables rotating model clockwise by 90 degrees
+import { Leva } from 'leva';
+import React, { Suspense, useMemo } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useMediaQuery } from 'react-responsive';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { useControls } from 'leva';
+import Button from '../components/Button.jsx';
+import CanvasLoader from '../components/CanvasLoader.jsx';
+import HeroCamera from '../components/HeroCamera.jsx';
+import { calculateSizes } from '../constants/index.js';
+import Statue from '../components/Statue.jsx';
 
 const Hero = () => {
+    // Use media queries to determine screen size
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-    const isSmall = useMediaQuery({ maxWidth: 440 })
-    const isMobile = useMediaQuery({ maxWidth: 768 })
-    const isTablet = useMediaQuery({ minWidth: 769, maxWidth: 1024 })
-    // manually calculated sizes from helper functions
-    const sizes = calculateSizes(isSmall, isMobile, isTablet)
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+    const options = useMemo(() => {
+        return {
+
+            scale: { value: 1, min: 0, max: Math.PI * 2, step: 0.01 },
+            positionX: { value: 0, min: -30, max: 30, step: 1 },
+            positionY: { value: 0, min: -30, max: 30, step: 1 },
+            positionZ: { value: 0, min: -30, max: 30, step: 1 },
+
+            visible: true,
+            color: { value: 'lime' },
+        }
+    }, [])
+
+    const cont = useControls('Pheonix', options)
 
     return (
-        <section className="min-h-screen w-full">
-            <div className="w-full h-full mx-auto flex flex-col sm:mt-36 mt-42 c-space gap-3 bg-transparent">
-                <p className="sm:text-3xl text-2xl font-medium text-text/80 text-center">Hi! <span className="waving-hand">👋</span> I'm Marina, a web-developer and a teacher based in Croatia.</p>
-                <p className="hero_tag gradient-text">Building web-sites, user-interfaces and more</p>
+        <section className="min-h-screen w-full flex flex-col relative" id="home">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+                    Hi, I am Marina <span className="waving-hand">👋</span>
+                </p>
+                <p className="hero_tag text-gray_gradient">Building web-sites, user-interfaces and more</p>
             </div>
-            <div className="w-full h-full inset-0 z-40">
+
+            <div className="w-full h-full absolute inset-0">
                 <Canvas className="w-full h-full">
                     <Suspense fallback={<CanvasLoader />}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
-                        <HeroCamera isMobile={isMobile}>
-                            <Pheonix scale={sizes.statueScale} position={[0, 0, 0]} rotation={[0, -0.1, 0]} />
-                        </HeroCamera>
+
+                        <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+
+                        <Statue scale={cont.scale} position={[cont.positionX, cont.positionY, cont.positionY]} rotation={[0.1, -Math.PI, 0]} visible={cont.visible} />
+
                         <ambientLight intensity={1} />
                         <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                        <OrbitControls />
                     </Suspense>
-
-                    <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
                 </Canvas>
-                {/* <Canvas className="w-full h-full">
-                    <Suspense fallback={<CanvasLoader />}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 10]} />
-                        <HeroCamera isMobile={isMobile}> 
-                        <Pheonix
-                            position={[0, 1, 0]}
-                            scale={1.5}
-                            rotation={[0, -Math.PI, 0]} />
-                        </HeroCamera>
-
-                        <ambientLight intensity={1} />
-                        <directionalLight position={[10, 10, 10]} intensity={0.5} />
-                    </Suspense>
-                </Canvas>  */}
             </div>
-            {/*  <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
-                <a href="#contact" className="w-fit">
-                    <Button name="Let's talk!" isBeam containerClass="sm:w-full w-fit sm:min-w-96" />
+
+            <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+                <a href="#about" className="w-fit">
+                    <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
                 </a>
-            </div> */}
+            </div>
         </section>
     );
-}
+};
 
 export default Hero;
